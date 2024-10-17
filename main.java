d8ae6f02f32d74c3fe6dab86b6ffd8f8ecf222e7
SNgomez27 <gomezsantus@gmail.com>
2024-10-17 11:45:20 +0200

    main con try

--- main.java
+++ main.java
@@ -1,7 +1,11 @@
 public class main{
 		public static void main (String[] args){
-		for (int i = 0 ;i < args.length;i++){  
+		for (int i = 0 ;i < args.length;i++){
+		try{
 		System.out.println("argumentos" + i + ":" + args[i]);
-	}
+	} catch (ArrayIndexOutOfBoundsException e){
+		System.out.println("error" + e.getMessage());
+}
+}
 }
 }
