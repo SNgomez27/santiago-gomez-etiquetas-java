2fe85544951e693b66e3554a3cbbedd4a574c3de
SNgomez27 <gomezsantus@gmail.com>
2024-10-17 11:21:03 +0200

    Solo el main.java

--- main.java
+++ main.java
@@ -1,6 +1,7 @@
-çpublic class Main{
-		public static  void main (String[]args){
-		for (int i = 0 ; args.length() < i,i++){
-		System.out.println("argumentos" + i + ":"args[i]);
+public class main{
+		public static void main (String[] args){
+		for (int i = 0 ;i < args.length;i++){
+		System.out.println("argumentos" + i + ":" + args[i]);
 	}
+}
 }
