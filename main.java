84e981fd2fe8db810d653267a6ae5134d8f81a27
SNgomez27 <gomezsantus@gmail.com>
2024-10-17 10:44:05 +0200

    impresion de argumenyos en main

--- main.java
+++ main.java
@@ -1,5 +1,9 @@
 public class Main{
 		public statis void main (String[]args){
-		system.out.println("hola mundo");
+		system.out.println(args.length);
+		if (args.legth ==0){
+			system.out.println ("No hay argumentos")
+		}else{
+		system.out.printlm("hay" + args.legth() + "argumentos");
 	}
-	}
+}
