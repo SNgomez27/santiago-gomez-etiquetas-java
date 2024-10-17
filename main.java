3a5c9abecbe9a23d0e8f2291aacb447f259f0623
SNgomez27 <gomezsantus@gmail.com>
2024-10-17 11:32:35 +0200

    EL MAIN CON LOS TAGS

--- main.java
+++ main.java
@@ -1,6 +1,6 @@
 public class main{
 		public static void main (String[] args){
-		for (int i = 0 ;i < args.length;i++){
+		for (int i = 0 ;i < args.length;i++){  
 		System.out.println("argumentos" + i + ":" + args[i]);
 	}
 }
